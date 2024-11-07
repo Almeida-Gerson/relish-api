60c19b5921bc8a94b07250054455ef3ef108a795
Almeida, Gerson (Bairesdev) <845066@bah.com>
2024-11-07 14:49:39 -0500

    feat - Move login to photoService

--- src/photo/photoController.js
+++ src/photo/photoController.js
@@ -1,97 +1,12 @@
-const NodeCache = require("node-cache");
-const photoService = require("./photoService");
-const { LIMIT, OFFSET } = require("../constants");
-const cache = new NodeCache({ stdTTL: 100, checkperiod: 300 });
+const {
+  getPhotos: getPhotosService,
+  getPhoto: getPhotoService,
+} = require("./photoService");
 
 // Get paginated photos based on filters sent by the user
 const getPhotos = async (req, res, next) => {
   try {
-    // Parse query parameters for filtering and pagination
-    const {
-      title: titleFilter,
-      "album.title": albumTitleFilter,
-      "album.user.email": userEmailFilter,
-    } = req.query || {};
-
-    // Pagination parameters
-    const limit = parseInt(req.query.limit) || LIMIT;
-    const offset = parseInt(req.query.offset) || OFFSET;
-
-    const cacheKey = `photos__titleFilter${titleFilter ?? ""}_albumTitleFilter${
-      albumTitleFilter ?? ""
-    }_userEmailFilter${userEmailFilter ?? ""}_limit${limit}_offset${offset}`;
-
-    // Getting a cache value
-    const cachedPhotos = cache.get(cacheKey);
-
-    if (cachedPhotos) {
-      res.status(200).json(cachedPhotos);
-      return;
-    }
-
-    // Fetch all data
-    const users = await photoService.getUsers();
-    const albums = await photoService.getAlbums();
-    const photos = await photoService.getPhotos();
-
-    // Enrich photos with album and user information
-    const enrichedPhotos = photos.map((photo) => {
-      const album = albums.find((album) => album.id === photo.albumId);
-      const user = users.find((user) => user.id === album.userId);
-
-      // Remove albumId property
-      const { albumId, ...newPhoto } = photo;
-
-      return {
-        ...newPhoto,
-        album: { id: album?.id, title: album?.title, user },
-      };
-    });
-
-    // Filter based on the provided query parameters
-    let filteredPhotos = enrichedPhotos;
-
-    if (titleFilter || albumTitleFilter || userEmailFilter) {
-      filteredPhotos = filteredPhotos.filter((photo) => {
-        // Filter by photo title
-        if (
-          titleFilter &&
-          !photo.title.toLowerCase().includes(titleFilter?.toLowerCase())
-        ) {
-          return false;
-        }
-
-        // Filter by album title
-        if (
-          albumTitleFilter &&
-          !photo.album.title
-            .toLowerCase()
-            .includes(albumTitleFilter?.toLowerCase())
-        ) {
-          return false;
-        }
-
-        // Filter by email
-        if (
-          userEmailFilter &&
-          !photo.album.user?.email
-            ?.toLowerCase()
-            .includes(userEmailFilter?.toLowerCase())
-        ) {
-          return false;
-        }
-
-        return true;
-      });
-    }
-
-    // Apply pagination
-    const paginatedPhotos = filteredPhotos.slice(offset, offset + limit);
-
-    const data = { photos: paginatedPhotos, total: filteredPhotos?.length };
-    // Setting cache value
-    cache.set(cacheKey, data);
-
+    const data = await getPhotosService(req.query || {});
     res.status(200).json(data);
   } catch (error) {
     next(error); // Pass error to the error handler
@@ -101,52 +16,8 @@ const getPhotos = async (req, res, next) => {
 // Get photos based on id sent by the user
 const getPhoto = async (req, res, next) => {
   try {
-    const id = Number(req.params.id);
-    if (!id) {
-      // Returns null if an invalid number is received
-      res.status(200).json(null);
-    }
-
-    const cacheKey = `photo__id${id}`;
-
-    // Getting a cache value
-    const cachedPhoto = cache.get(cacheKey);
-
-    if (cachedPhoto) {
-      res.status(200).json(cachedPhoto);
-      return;
-    }
-
-    // Fetch all data
-    const photos = await photoService.getPhotos({
-      id: req.params.id,
-    });
-    const [firstPhoto] = photos || [];
-    const { albumId: firstPhotoAlbumId } = firstPhoto || {};
-    const albums = await photoService.getAlbums({ id: firstPhotoAlbumId });
-    const [firstAlbum] = albums || [];
-    const { userId, title: albumTitle } = firstAlbum || {};
-    const users = await photoService.getUsers({
-      id: userId,
-    });
-    const [firstUser] = users || [];
-
-    // Remove albumId property
-    const { albumId, ...photo } = firstPhoto || {};
-
-    // Enrich photo with album and user information
-    const enrichedPhoto = {
-      ...photo,
-      album: {
-        id: firstPhotoAlbumId,
-        title: albumTitle,
-        user: firstUser,
-      },
-    };
-
-    // Setting cache value
-    cache.set(cacheKey, enrichedPhoto);
-    res.status(200).json(enrichedPhoto);
+    const data = await getPhotoService(req.params.id);
+    res.status(200).json(data);
   } catch (error) {
     next(error); // Pass error to the error handler
   }
--- src/photo/photoService.js
+++ src/photo/photoService.js
@@ -1,22 +1,228 @@
+const NodeCache = require("node-cache");
 const axios = require("axios");
+const { OFFSET, LIMIT } = require("../constants");
+
+const cache = new NodeCache({ stdTTL: 100, checkperiod: 300 });
 
 const instance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
 });
 
-const getPhotos = async (params) => {
+const getPhotosService = async (params) => {
   const response = await instance.get("/photos", { params });
   return response.data;
 };
 
-const getAlbums = async (params) => {
+const getAlbumsService = async (params) => {
   const response = await instance.get("/albums", { params });
   return response.data;
 };
 
-const getUsers = async (params) => {
+const getUsersService = async (params) => {
   const response = await instance.get("/users", { params });
   return response.data;
 };
 
-module.exports = { getPhotos, getAlbums, getUsers };
+// Enrich photos with album and user information
+const enrichPhotos = (photos = [], albums = [], users = []) => {
+  const enrichedPhotos = photos.map((photo) => {
+    const album = albums.find((album) => album.id === photo.albumId);
+    const user = users.find((user) => user.id === album.userId);
+
+    // Remove albumId property
+    const { albumId, ...newPhoto } = photo;
+
+    return {
+      ...newPhoto,
+      album: { id: album?.id, title: album?.title, user },
+    };
+  });
+
+  return enrichedPhotos;
+};
+
+// Filter based on provided parameters
+const filterPhotos = (
+  photos = [],
+  filters = { title: "", albumTitle: "", userEmail: "" }
+) => {
+  const { title, albumTitle, userEmail } = filters || {};
+
+  if (!title && !albumTitle && !userEmail) {
+    return photos;
+  }
+
+  const filteredPhotos = photos.filter((photo) => {
+    // Filter by photo title
+    if (title && !photo.title.toLowerCase().includes(title?.toLowerCase())) {
+      return false;
+    }
+
+    // Filter by album title
+    if (
+      albumTitle &&
+      !photo.album.title.toLowerCase().includes(albumTitle?.toLowerCase())
+    ) {
+      return false;
+    }
+
+    // Filter by email
+    if (
+      userEmail &&
+      !photo.album.user?.email?.toLowerCase().includes(userEmail?.toLowerCase())
+    ) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return filteredPhotos;
+};
+
+const paginatePhotos = (
+  photos = [],
+  filters = { offset: OFFSET, limit: LIMIT }
+) => {
+  const { limit, offset } = filters || {};
+
+  // Apply pagination
+  const paginatedPhotos = photos?.slice(offset, offset + limit);
+
+  const data = { photos: paginatedPhotos, total: photos?.length };
+
+  return data;
+};
+
+// Get paginated photos based on filters sent by the user
+const getPhotos = async (
+  filters = {
+    title: "",
+    "album.title": "",
+    "album.user.email": "",
+    limit: LIMIT,
+    offset: OFFSET,
+  }
+) => {
+  try {
+    // Parse query parameters for filtering and pagination
+    const {
+      title,
+      "album.title": albumTitle,
+      "album.user.email": userEmail,
+      limit,
+      offset,
+    } = filters || {};
+
+    // Pagination parameters
+    const parsedLimit = parseInt(limit, 10);
+    const parsedOffset = parseInt(offset, 10);
+
+    const cacheKey = "enrichedPhotos";
+
+    // Getting a cache value
+    const cachedPhotos = cache.get(cacheKey);
+
+    if (cachedPhotos) {
+      // Filter based on the provided query parameters
+      const filteredPhotos = filterPhotos(cachedPhotos, {
+        title,
+        albumTitle,
+        userEmail,
+      });
+
+      // Apply pagination
+      const data = paginatePhotos(filteredPhotos, {
+        limit: parsedLimit,
+        offset: parsedOffset,
+      });
+
+      return data;
+    }
+
+    // Fetch all data
+    const users = await getUsersService();
+    const albums = await getAlbumsService();
+    const photos = await getPhotosService();
+
+    // Enrich photos with album and user information
+    const enrichedPhotos = enrichPhotos(photos, albums, users);
+
+    // Setting cache value
+    cache.set(cacheKey, enrichedPhotos);
+
+    // Filter based on the provided query parameters
+    const filteredPhotos = filterPhotos(enrichedPhotos, {
+      title,
+      albumTitle,
+      userEmail,
+    });
+
+    // Apply pagination
+    const data = paginatePhotos(filteredPhotos, {
+      limit: parsedLimit,
+      offset: parsedOffset,
+    });
+
+    return data;
+  } catch (error) {
+    throw error; // Pass error to the error handler
+  }
+};
+
+// Get photos based on id sent by the user
+const getPhoto = async (photoId) => {
+  try {
+    const id = Number(photoId);
+    if (!id) {
+      // Returns null if an invalid number is received
+      return null;
+    }
+
+    const cacheKey = `photo__id${id}`;
+
+    // Getting a cache value
+    const cachedPhotos = cache.get(cacheKey);
+
+    if (cachedPhotos) {
+      return cachedPhotos;
+    }
+
+    // Fetch all data
+    const photos = await getPhotosService({
+      id,
+    });
+    const [firstPhoto] = photos || [];
+    const { albumId: firstPhotoAlbumId } = firstPhoto || {};
+    const albums = await getAlbumsService({
+      id: firstPhotoAlbumId,
+    });
+    const [firstAlbum] = albums || [];
+    const { userId, title: albumTitle } = firstAlbum || {};
+    const users = await getUsersService({
+      id: userId,
+    });
+    const [firstUser] = users || [];
+
+    // Remove albumId property
+    const { albumId, ...photo } = firstPhoto || {};
+
+    // Enrich photo with album and user information
+    const enrichedPhoto = {
+      ...photo,
+      album: {
+        id: firstPhotoAlbumId,
+        title: albumTitle,
+        user: firstUser,
+      },
+    };
+
+    // Setting cache value
+    cache.set(cacheKey, enrichedPhoto);
+    return enrichedPhoto;
+  } catch (error) {
+    throw error; // Pass error to the error handler
+  }
+};
+
+module.exports = { getPhotos, getPhoto };
